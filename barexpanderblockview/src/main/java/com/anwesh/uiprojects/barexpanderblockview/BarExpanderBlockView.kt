1846cad7c39b0f459155dd6f0730ffc722e1f5d0
Anwesh43 <anweshthecool0@gmail.com>
2020-08-06 02:31:17 +0530

    adding Canvas extension method

--- barexpanderblockview/src/main/java/com/anwesh/uiprojects/barexpanderblockview/BarExpanderBlockView.kt
+++ barexpanderblockview/src/main/java/com/anwesh/uiprojects/barexpanderblockview/BarExpanderBlockView.kt
@@ -16,7 +16,6 @@ import android.app.Activity
 val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#F44336", "#03A9F4", "#FFEB3B")
 val parts : Int = 3
 val scGap : Float = 0.02f / parts
-val strokeFactor : Int = 90
 val sizeFactor : Float = 3.5f
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
@@ -26,3 +25,26 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBarExpanderBlock(i : Int, scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, 2)
+    val sf2 : Float = sf.divideScale(1, 2)
+    save()
+    scale(1f - 2 * i, 1f - 2 * i)
+    translate(-w / 2, 0f)
+    drawRect(RectF(0f, -size / 2, size * sf1, size / 2), paint)
+    drawRect(RectF(-(w / 2 - size) * sf2, -size / 2, 0f, size / 2), paint)
+    restore()
+}
+
+fun Canvas.drawBEBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    save()
+    translate(w / 2, h / 2)
+    drawBarExpanderBlock(i, scale, w, h, paint)
+    restore()
+}
