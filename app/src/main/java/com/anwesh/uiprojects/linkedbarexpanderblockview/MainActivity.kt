c2d14d4af29f113221fbafcd8ac1d3c595d68224
Anwesh43 <anweshthecool0@gmail.com>
2020-08-06 02:58:17 +0530

    adding view and fullscreen support

---
+++ app/src/main/java/com/anwesh/uiprojects/linkedbarexpanderblockview/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.anwesh.uiprojects.linkedbarexpanderblockview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.barexpanderblockview.BarExpanderBlockView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        BarExpanderBlockView.create(this)
+        fullScreen()
+    }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+}
